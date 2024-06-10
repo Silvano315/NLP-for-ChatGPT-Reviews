7bd8071ab2adefddc75d9133b1f767f8d4ababf4
Silvano Quarto <silvano.quarto@gmail.com>
2024-06-10 11:11:53 +0200

    functions integration

--- src/data_viz_functions.py
+++ src/data_viz_functions.py
@@ -1,6 +1,14 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+from wordcloud import WordCloud
+from collections import Counter
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
 
 # Plots a bar chart for a specified numerical feature 
 def plot_bar(df, feature, num_bars_max=None):
@@ -26,4 +34,34 @@ def plot_bar(df, feature, num_bars_max=None):
     plt.xlabel(f'{feature}')
     plt.ylabel('Count')
     plt.tight_layout()
+    plt.show()
+
+
+# Word Cloud to visualize the most frequent and important words 
+def plot_word_cloud(text):
+
+    wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
+
+    plt.figure(figsize=(10, 5))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.show()
+
+
+# Bar Plot of Top N Words
+def plot_top_n_words(text, n=20):
+
+    stop_words = set(stopwords.words('english'))
+    words = word_tokenize(text.lower())
+    filtered_words = [word for word in words if word.isalnum() and word not in stop_words]
+    word_counts = Counter(filtered_words)
+    common_words = word_counts.most_common(n)
+    
+    words, counts = zip(*common_words)
+
+    plt.figure(figsize=(10, 5))
+    sns.barplot(x=list(counts), y=list(words), palette='viridis', hue=list(words), legend=False)
+    plt.title(f'Top {n} Most Common Words')
+    plt.xlabel('Counts')
+    plt.ylabel('Words')
     plt.show()
