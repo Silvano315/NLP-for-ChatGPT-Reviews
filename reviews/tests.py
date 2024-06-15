a10af5a783b96a7daad5f195dc9f1fc95d7b15f6
Silvano Quarto <silvano.quarto@gmail.com>
2024-06-15 11:27:12 +0200

    Docker app integration

---
+++ reviews/tests.py
@@ -0,0 +1,7 @@
+# /Users/silvanoquarto/Desktop/PROJECTS/NLP-for-ChatGPT-Reviews/reviews/tests.py
+
+from django.test import TestCase
+
+class SimpleTest(TestCase):
+    def test_addition(self):
+        self.assertEqual(1 + 1, 2)
